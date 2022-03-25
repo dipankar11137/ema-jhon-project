5074704c93fecd28be01714bf677fe4635ade2c8
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-25 11:55:56 +0600

    add cart

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
@@ -8,12 +9,14 @@ const Shop = () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
 
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>this is product {products.length}</h3>
+                {
+                    products.map(product => <Product key={product.id} product={product}></Product>)
+                }
 
             </div>
             <div className="cart-container">
